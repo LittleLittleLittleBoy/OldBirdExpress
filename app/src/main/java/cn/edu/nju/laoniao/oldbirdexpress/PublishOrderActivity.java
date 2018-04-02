ea8e4cd5f91b97fa59fe9bdc4133f7b55a4b3bb5
LittleLittleLittleBoy <151250082@smail.nju.edu.cn>
2018-04-02 08:53:42 +0800

    publish and get orders

--- app/src/main/java/cn/edu/nju/laoniao/oldbirdexpress/PublishOrderActivity.java
+++ app/src/main/java/cn/edu/nju/laoniao/oldbirdexpress/PublishOrderActivity.java
@@ -8,23 +8,46 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import cn.edu.nju.laoniao.oldbirdexpress.model.Order;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PublishOrderActivity extends AppCompatActivity {
 
+    private static final String TAG = "PublishOrderActivity";
+
     private RecyclerView recyclerView;
+
+    private Timer timer;
+
+    private final String url="http:172.19.112.245:8080/getNewOrders?user=b";
+
+    private MyViewAdapter myViewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,8 +55,76 @@ public class PublishOrderActivity extends AppCompatActivity {
         setContentView(R.layout.activity_publish_order_list);
         recyclerView=findViewById(R.id.my_publish_order_list);
         recyclerView.setLayoutManager(new LinearLayoutManager(PublishOrderActivity.this));
-        recyclerView.setAdapter(new MyViewAdapter());
+
+        myViewAdapter=new MyViewAdapter();
+        recyclerView.setAdapter(myViewAdapter);
+
+        createTimerTask();
     }
+
+    private void createTimerTask() {
+        timer=new Timer();
+        TimerTask alermTask=new AlarmTask();
+        timer.schedule(alermTask,0,500);
+    }
+
+
+    public class AlarmTask extends TimerTask{
+
+        @Override
+        public void run() {
+            Log.e("de", "run: " );
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request request = new Request
+                    .Builder()
+                    .get()
+                    .url(url)
+                    .build();
+            okHttpClient.newCall(request).enqueue(new Callback() {
+
+                @Override
+                public void onFailure(Call call, IOException e) {
+                    Log.e(TAG, e.getMessage());
+                }
+
+                @RequiresApi(api = Build.VERSION_CODES.O)
+                @Override
+                public void onResponse(Call call, Response response) {
+                    try {
+                        String result=response.body().string();
+
+                        final List<Order> stus = new ArrayList<Order>();
+                        JsonParser parser = new JsonParser();//创建一个JsonParse
+                        //通过JsonParser对象可以把json格式的字符串解析成一个JsonElement对象，然后通过JsonElement对象生成JsonArray序列
+                        JsonArray jarray = parser.parse(result).getAsJsonArray();
+                        Gson gson=new Gson();
+                        //通过JsonArray把对象从中提取到List集合中来
+                        for (JsonElement jsonElement : jarray) {
+                            stus.add(gson.fromJson(jsonElement, Order.class));
+                        }
+                        if (stus.size()!=myViewAdapter.getItemCount()){
+                            runOnUiThread(new Runnable() {
+                                @RequiresApi(api = Build.VERSION_CODES.O)
+                                @Override
+                                public void run() {
+                                    myViewAdapter.setOrders(stus);
+                                    myViewAdapter.notifyDataSetChanged();
+                                }
+                            });
+
+                        }
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+
+            });
+
+        }
+    }
+
 
     private class ViewHolder extends RecyclerView.ViewHolder {
 
@@ -82,16 +173,19 @@ public class PublishOrderActivity extends AppCompatActivity {
     private class MyViewAdapter extends RecyclerView.Adapter<ViewHolder>{
 
         List<Order> orders=new ArrayList<>();
-        {
-            orders.add(new Order("0001","小明","","那经大学","菜鸟一站","南苑六社", LocalDateTime.now(),LocalTime.now(),LocalTime.now(),100,"enen","ahah",0));
-            orders.add(new Order("0002","小明","","那经大学","菜鸟一站","南苑六社",LocalDateTime.now(),LocalTime.now(),LocalTime.now(),100,"","",0));
-
-        }
         @Override
         public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             LayoutInflater layoutInflater = LayoutInflater.from(PublishOrderActivity.this);
 
             return new ViewHolder(layoutInflater, parent);
+        }
+
+        public void setOrders(List<Order> list){
+            this.orders=list;
+        }
+
+        public List<Order> getOrders() {
+            return orders;
         }
 
         @Override
